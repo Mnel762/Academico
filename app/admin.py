5dd50e04e1fe98620556bca0a633436329748fe8
Mnel762 <emannuelsil15@gmail.com>
2025-05-19 16:50:59 -0300

    Add files via upload

---
+++ app/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import *
+
+# Registro das models no admin
+admin.site.register(Cidade)
+admin.site.register(Ocupacao)
+admin.site.register(Pessoa)
+admin.site.register(InstituicaoEnsino)
+admin.site.register(AreaSaber)
+admin.site.register(Curso)
+admin.site.register(Turma)
+admin.site.register(Disciplina)
+admin.site.register(Matricula)
+admin.site.register(AvaliacaoTipo)
+admin.site.register(Avaliacao)
+admin.site.register(Frequencia)
+admin.site.register(Turnos)
+admin.site.register(Ocorrencia)
+admin.site.register(CursoDisciplina)
